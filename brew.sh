5095dea48c4660304825de3d9d5a92daebf71c99
khalidbelk <me@khal1d.com>
2026-08-30 06:50:03 +0200

    chore(script): fix warnings raised by brew style

--- brew.sh
+++ brew.sh
@@ -5,52 +5,56 @@ usage() {
   exit 1
 }
 
-[ $# -eq 2 ] || usage
+[[ $# -eq 2 ]] || usage
 
 formula="$1"
 action="$2"
-qualified="khalidbelk/taps/$formula"
-file="Formula/$formula.rb"
+qualified="khalidbelk/taps/${formula}"
+file="Formula/${formula}.rb"
 
-[ -f "$file" ] || { echo "Error: $file not found" >&2; exit 1; }
+[[ -f "${file}" ]] || {
+  echo "Error: ${file} not found" >&2
+  exit 1
+}
 
 install_or_reinstall() {
-  if brew list --formula "$qualified" >/dev/null 2>&1; then
-    brew reinstall "$qualified"
+  if brew list --formula "${qualified}" >/dev/null 2>&1
+  then
+    brew reinstall "${qualified}"
   else
-    brew install "$qualified"
+    brew install "${qualified}"
   fi
 }
 
 all() {
-    echo "[----- STYLE CHECK -----]\n"
-    brew style "$file"
-    echo "\n"
+  printf "[----- STYLE CHECK -----]\n"
+  brew style "${file}"
+  printf "\n"
 
-    echo "[-----  AUDITING   -----]\n"
-    brew audit --strict --online "$qualified"
+  printf "[-----  AUDITING   -----]\n"
+  brew audit --strict --online "${qualified}"
 
-    echo "[-----  INSTALLING  -----]\n"
-    install_or_reinstall
-    echo "\n"
+  printf "[-----  INSTALLING  -----]\n"
+  install_or_reinstall
+  printf "\n"
 
-    echo "[-----  TESTING  -----]\n"
-    brew test "$qualified"
+  printf "[-----  TESTING  -----]\n"
+  brew test "${qualified}"
 }
 
-case "$action" in
+case "${action}" in
   style)
-    brew style "$file"
+    brew style "${file}"
     ;;
   audit)
-    brew audit --strict --online "$qualified"
+    brew audit --strict --online "${qualified}"
     ;;
   install)
     install_or_reinstall
     ;;
   test)
     install_or_reinstall
-    brew test "$qualified"
+    brew test "${qualified}"
     ;;
   all) all ;;
   *)
